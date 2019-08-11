d1accae8e0fd501593400aa490979423d33f77e4
Liss Heidrich <lissheidr@gmail.com>
2019-08-11 11:58:16 +0200

    Added SELinux Policies and corrected Permissions

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-if mkdir /opt/mcserv/scripts; then
-    cp -r scripts /opt/mcserv/scripts
-    cp systemd-units/* /etc/systemd/system
-fi
+cp -r scripts /opt/mcserv
+cp systemd-units/* /etc/systemd/system
+
