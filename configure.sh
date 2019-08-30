94f1d5c63497541fc44c8888908c5d3d38a9a211
Liss <31625940+Clueliss@users.noreply.github.com>
2019-08-30 20:44:37 +0200

    added configuration script

---
+++ configure.sh
@@ -0,0 +1,11 @@
+
+if [[ $# < 1 ]]; then
+    echo "Usage: configure PATH_TO_MCSERVER"
+    exit 1
+fi
+
+to_edit=(systemd-units/mcserv.service systemd-units/mcserv-stoptimerctrl.service)
+
+for f in "${to_edit[@]}"; do
+    sed -i 's!/opt/mcserv!'$1'!g' "$f"
+done
