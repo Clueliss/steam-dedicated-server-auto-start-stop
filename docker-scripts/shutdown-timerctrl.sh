731f675df78cdae1e8d048c108b1fbbbc68b3e3f
Liss <31625940+Clueliss@users.noreply.github.com>
2019-09-07 18:53:37 +0200

    fixed script (now works; hopefully?)

--- docker-scripts/shutdown-timerctrl.sh
+++ docker-scripts/shutdown-timerctrl.sh
@@ -8,35 +8,31 @@ stop_timer() {
     systemctl stop mcserv-stop.timer
 }
 
-init_player_count() {
-    declare -i cnt=0
+init_num_players() {
+    declare -i num_joins=$(docker logs minecraft | grep "joined the game" | wc -l)
+    declare -i num_leaves=$(docker logs minecraft | grep "left the game" | wc -l)
 
-    docker logs minecraft | while read line; do
-        if echo $line | grep -q "joined the game"; then
-            local cnt=$cnt + 1
-        elif echo $line | grep -q "left the game"; then
-            local cnt=$cnt - 1
-        fi
-    done
-
-    echo $cnt
+    echo $(($num_joins - $num_leaves))
 }
 
+players_online=$(init_num_players)
 
-declare timer_running=false
-declare -i players_online=$(init_player_count)
-
-if [[ players_online == 0 ]]; then
+if [[ $players_online == 0 ]]; then
     start_timer
     timer_running=true
 
-    echo "[Info] Started Timer"
+    echo "[Info] Timer Running"
+else
+   stop_timer
+   timer_running=false
+
+   echo "[Info] Timer Stopped"
 fi
 
 
 docker logs minecraft --follow --since 0s | while read line; do
     if echo "$line" | grep -q "joined the game"; then
-        num_players=$num_players + 1
+        players_online=$(($players_online + 1))
 
         if [[ $timer_running == true ]]; then
             stop_timer
@@ -44,14 +40,14 @@ docker logs minecraft --follow --since 0s | while read line; do
 
             echo "[Info] Stopped kill timer"
         fi
-
+    
     elif echo "$line" | grep -q "left the game"; then
-        num_players=$num_players - 1
+        players_online=$(($players_online - 1))
 
-        if [[ $num_players == 0 ]] && [[ $timer_running == false ]]; then
+        if [[ $players_online == 0 ]] && [[ $timer_running == false ]]; then
             start_timer
             timer_running=true
-            
+
             echo "[Info] Started kill timer"
         fi
     fi
