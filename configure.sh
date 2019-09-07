f75f8e8ea38929bf7426007f0d209aad4b0b2197
Liss <31625940+Clueliss@users.noreply.github.com>
2019-09-07 18:52:57 +0200

    added help, and fixed that it would not modify install.sh and uninstall.sh

--- configure.sh
+++ configure.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [[ $# < 1 ]]; then
+if [[ $# < 1 ]] || [[ $1 == "--help" ]] || [[ $1 == "-h" ]]; then
     echo "Usage: configure PATH_TO_MCSERVER"
     exit 1
 fi
 
-to_edit=(systemd-units/mcserv.service systemd-units/mcserv-stoptimerctrl.service)
+to_edit=(install.sh uninstall.sh systemd-units/mcserv.service systemd-units/mcserv-stoptimerctrl.service)
 
 for f in "${to_edit[@]}"; do
     sed -i 's!/opt/mcserv!'$1'!g' "$f"
